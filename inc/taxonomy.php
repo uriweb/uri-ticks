e68ba1e675e2eb5c0cdd9d56b5f32eff2ff5d5f1
Brandon Fuller <bjcfuller@uri.edu>
2020-02-07 12:28:16 -0500

    Add region taxonomy for diseases

--- inc/taxonomy.php
+++ inc/taxonomy.php
@@ -36,3 +36,36 @@ function uri_ticks_vector_taxonomy() {
 
 }
 add_action( 'init', 'uri_ticks_vector_taxonomy' );
+
+
+/**
+ * Define the region taxonomy for disease post types
+ */
+function uri_ticks_region_taxonomy() {
+
+	register_taxonomy(
+	'region',
+	array(
+		0 => 'disease',
+	),
+	array(
+		'hierarchical' => true,
+		'label' => 'Regions',
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => false,
+		'show_in_rest' => true,
+		'singular_label' => 'Region',
+		'labels' => array(
+			'add_new_item' => __( 'Add New Region' ),
+			'update_item' => __( 'Update Region' ),
+			'view_item' => __( 'View Region' ),
+			'edit_item' => __( 'Edit Region' ),
+			'all_item' => __( 'All Regions' ),
+		),
+	)
+	);
+
+}
+add_action( 'init', 'uri_ticks_region_taxonomy' );
