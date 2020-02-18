92f1d78fe0f4187d6d5887ea526e7d2b79a26631
Brandon Fuller <bjcfuller@uri.edu>
2020-02-18 09:07:12 -0500

    Add a basic layout framework for the map

--- inc/tick-map.php
+++ inc/tick-map.php
@@ -19,13 +19,39 @@ function uri_ticks_shortcode_tick_map( $atts, $content = null ) {
 	   $atts
 	   );
 
-	ob_start();
-
-	include( URI_TICKS_DIR_PATH . '/i/us_states.svg' );
-
-	$output = ob_get_clean();
-
-	return $output;
+	return uri_ticks_map_build_output( $atts );
 
 }
 add_shortcode( 'uri-tick-map', 'uri_ticks_shortcode_tick_map' );
+
+
+/**
+ * Build the output
+ *
+ * @param arr $atts the attributes.
+ */
+function uri_ticks_map_build_output( $atts ) {
+
+	ob_start();
+
+	?>
+
+	<div id="uri-tick-map-wrapper">
+		<div class="map-container">
+			<?php include( URI_TICKS_DIR_PATH . '/i/us_states.svg' ); ?>
+		</div>
+		<div class="species-container">
+			<h2>Species</h2>
+			<div class="species-list">
+			</div>
+		</div>
+		<div class="time-slider">
+			<p>Time slider</p>
+		</div>
+	</div>
+
+	<?php
+
+	return ob_get_clean();
+
+}
